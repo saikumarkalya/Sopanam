0b3caa0773f8a7640171fdabc3a57ae03d49be3f
saikumarkalya <saikumarkalya@gmail.com>
2022-04-26 22:52:14 -0700

    First Cut
    First Cut

---
+++ js/index.js
@@ -0,0 +1,7 @@
+alert("This is main index page");
+
+myFunction()
+function myFunction() {
+    alert("This is click event in index page");
+  /*  document.getElementById("demo").style.color = "red"; */
+  }
